30f24d569ecd82cf1ec2b3b277004257399a6885
Paul Sokolov Jr <sokolov.p64@gmail.com>
2018-09-29 01:04:37 +0300

    +CyclicWrapper, +docs, tab -> 4 spaces
    Signed-off-by: Paul Sokolov Jr <sokolov.p64@gmail.com>

--- functors-and-callbacks.js
+++ functors-and-callbacks.js
@@ -1,187 +1,347 @@
 function LinearObject(f, wait, ...argsF) {
-	var ret = function() {
-		return ret.apply(...arguments);
-	};
 
-	var next;
+    var ret = function LinearObject() {
+        return ret.apply(...arguments);
+    };
 
-	ret.setNext = function(g, w, ...argsG) {
-		return next = new LinearObject(g, w, ...argsG);
-	};
+    var next;
 
-	ret.apply = function() {
-		if (!next)
-			return f(...argsF, ...arguments);
-		if (!wait)
-			return next.apply(f(...argsF, ...arguments));
-		return f(...argsF, ...arguments, next);
-	};
+    ret.setNext = function(g, w, ...argsG) {
+        return next = new LinearObject(g, w, ...argsG);
+    };
 
-	return ret;
+    ret.apply = function() {
+        if (!next)
+            return f(...argsF, ...arguments);
+        if (!wait)
+            return next.apply(f(...argsF, ...arguments));
+        return f(...argsF, ...arguments, next);
+    };
+
+    return ret;
 };
 
 /**
  * Wrapper for "Linear" operations. Queues functors one after another.
- * Arguments of functors w/out callbacks must be the following: ([...prearranged], [last]), where 'last' is return from previous functor (or launch arguments, if first)
- * Arguments of functors with callbacks must be the following: ([...prearranged], [last], [next]), where 'next' is next functor
+ *
  * @kind class
- * @param {functor} f first functor in queue
- * @param {Boolean} wait tells whether functor f contains a callback
+ *
+ * @param {function} f first functor in queue
+ * @param {Boolean} waitF true if functor f contains a callback, false otherwise
  * @param {...Object} argsF preset params for the functor f
- * @returns {function} function so Wrapper object can be used as functor itself
+ *
+ * If first functor doesn't have a callback, it must match this scheme:
+ * * @param {...Object} argsF
+ * * @param {...Object} arguments .apply() arguments
+ * * @returns {Object} will be given to the next functor as tail argument
+ *
+ * If first functor has a callback, it must match this scheme:
+ * * @param {...Object} argsF
+ * * @param {...Object} arguments .apply() arguments
+ * * @param {function} next functor
+ *
+ * @returns {function} so Wrapper object can be used as functor itself
  */
-function LinearWrapper(f, wait, ...argsF) {
-	var ret = function() {
-		return ret.apply(...arguments);
-	};
+function LinearWrapper(f, waitF, ...argsF) {
 
-	const first = new LinearObject(f, wait, ...argsF);
-	  var last  = first;
+    var ret = function LinearWrapper() {
+        return ret.apply(...arguments);
+    };
 
-	/**
-	 * Method to queue functors
-	 * @param {functor} g next functor in queue
-	 * @param {Boolean} w tells whether functor g contains a callback
-	 * @param {...Object} argsG preset params for the functor g
-	 * @returns {LinearWrapper} this Wrapper
-	 */
-	ret.andThen = function(g, w, ...argsG) {
-		last = last.setNext(g, w, ...argsG);
-		return ret;
-	};
+    const first = new LinearObject(f, waitF, ...argsF);
+      var last  = first;
 
-	/**
-	 * Method to apply all the queued functors. Works the same as function returned from constructor.
-	 * @param {...Object} arguments launch arguments ('last' for the first functor)
-	 * @returns {Object} return from the last functor\
-	 */
-	ret.apply = function() { return first.apply(...arguments); };
+    /**
+     * Method to queue functors.
+     *
+     * @param {function} g next functor in queue
+     * @param {Boolean} waitG true if functor g contains a callback, false otherwise
+     * @param {...Object} argsG preset params for the functor g
+     *
+     * If added functor doesn't have a callback, it must match this scheme:
+     * * @param {...Object} argsF
+     * * @param {Object} tail any result from previous functor (passed in callback or returned from non-callback)
+     * * @returns {Object} will be given to the next functor as tail argument
+     *
+     * If first functor has a callback, it must match this scheme:
+     * * @param {...Object} argsF
+     * * @param {Object} tail any result from previous functor (passed in callback or returned from non-callback)
+     * * @param {function} next functor
+     *
+     * @returns {LinearWrapper} this Wrapper
+     */
+    ret.andThen = function(g, waitG, ...argsG) {
+        last = last.setNext(g, waitG, ...argsG);
+        return ret;
+    };
 
-	return ret;
+    /**
+     * Method to apply all the queued functors. Works the same as function returned from constructor.
+     *
+     * @param {...Object} arguments launch arguments
+     *
+     * @returns {Object} return from the first callback functor (or from last functor, if none is callback)
+     */
+    ret.apply = function() { return first.apply(...arguments); };
+
+    return ret;
 };
 
 /**
- * Wrapper for "Scenario" operations (switcher). Selects the next functor depending on result from first one.
- * Arguments for first functor must be the following: ([...prearranged], [...launch], [map]), where map is selection map (if first functor contains a callback)
- * If first functor doesn't have a callback, it must return an Object which is pair of Key and nextArgs, where Key is key of next functor and nextArgs (which is Array) are given to next functor
+ * Wrapper for "Scenario" operations (switcher). Selects the next functor depending on result from root functor.
+ *
  * @kind class
- * @param {functor} f first functor
+ *
+ * @param {function} f root functor
  * @param {Boolean} wait tells whether functor f contains a callback
  * @param {...Object} argsF preset params for the functor f
- * @returns {function} function so Wrapper object can be used as functor itself
+ *
+ * If first functor doesn't have a callback, it must match this scheme:
+ * * @param {...Object} argsF
+ * * @param {...Object} arguments .apply() arguments
+ * * @returns {Object} { key: Object, args: Array } the next functor will be got by key and given the ...args as arguments
+ *
+ * If first functor has a callback, it must match this scheme:
+ * * @param {...Object} argsF
+ * * @param {...Object} arguments .apply() arguments
+ * * @param {Map} map from keys (possible outcomes of root functor execution) to next functor variations
+ *
+ * @returns {function} so Wrapper object can be used as functor itself
  */
 function ScenarioWrapper(f, wait, ...argsF) {
-	var ret = function() {
-		return ret.apply(...arguments);
-	};
-		
-	var links = new Map();
 
-	/**
-	 * Sets selection map
-	 * functors which are values of this map must accept ...nextArgs (a spread of Array), and nextArgs is the second elem from the pair returned by first functor
-	 * @param {Map} links_map selection map. The value (functor) from map is called if the key equals to the first elem from the return value of first functor
-	 * @returns {ScenarioWrapper} this Wrapper
-	 */
-	ret.setLinks = function(links_map) {
-		links = new Map(links_map);
-		return ret;
-	};
+    var ret = function ScenarioWrapper() {
+        return ret.apply(...arguments);
+    };
 
-	/**
-	 * Drops selection map
-	 * @returns {ScenarioWrapper} this Wrapper
-	 */
-	ret.dropLinks = function() {
-		links = new Map();
-		return ret;
-	};
+    var links = new Map();
 
-	/**
-	 * Adds new option
-	 * @param {Object} key a result that must be returned from first functor in order to execute this option
-	 * @param {functor} functor the option itself
-	 * @returns {ScenarioWrapper} this Wrapper
-	 */
-	ret.addLink = function(key, functor) {
-		links.set(key, functor);
-		return ret;
-	};
+    /**
+     * Sets selection map.
+     *
+     * @param {Map} links_map selection map. The value (functor) from map to execute is got by key --- main result (as return or in callback) of root functor
+     *
+     * Functors which are values of this map must match this scheme:
+     * * @param {...Object} args spread of any result (as return or passed in callback) from root functor that comes in pair with key
+     * * @returns {Object} anything
+     *
+     * @returns {ScenarioWrapper} this Wrapper
+     */
+    ret.setLinks = function(links_map) {
+        links = new Map(links_map);
+        return ret;
+    };
 
-	/**
-	 * Gets an option functor by key
-	 * @param {Object} key result that must be returned from first functor in order to execute this option
-	 * @returns {Object} a pair of this Wrapper and option functor
-	 */
-	ret.getLink = function(key) {
-		return { wrapper: ret, ret: links.get(key) };
-	};
+    /**
+     * Drops selection map.
+     */
+    ret.dropLinks = function() {
+        links = new Map();
+    };
 
-	/**
-	 * Removes an option by key
-	 * @param {Object} key result that would execute this option if it was reached by return from first functor
-	 * @returns {Object} a pair of this Wrapper and Boolean, which tells whether deletion occured
-	 */
-	ret.removeLink = function(key) {
-		return { wrapper: ret, ret: links.delete(key) };
-	};
+    /**
+     * Adds new option.
+     *
+     * @param {Object} key main result (as return or in callback) of root functor that should map to new option
+     * @param {function} functor the option itself
+     *
+     * Functor must match this scheme:
+     * * @param {...Object} args spread of any result (as return or passed in callback) from root functor that comes in pair with key
+     * * @returns {Object} anything
+     *
+     * @returns {ScenarioWrapper} this Wrapper
+     */
+    ret.addLink = function(key, functor) {
+        links.set(key, functor);
+        return ret;
+    };
 
-	/**
-	 * Applies first functor and the one which executes from the first functor's result. Works the same as function returned from constructor.
-	 * @param {...Object} arguments launch arguments
-	 * @returns {Object} a return from the last executed functor
-	 */
-	ret.apply = function() {
-		if (links.size == 0)
-			return f(...argsF, ...arguments);
-		if (!wait) {
-			let { next, argsNext } = f(...argsF, ...arguments);
-			return links.get(next)(...argsNext);
-		}
-		return f(...argsF, ...arguments, links);
-	};
+    /**
+     * Gets an option functor by key.
+     *
+     * @param {Object} key main result (as return or passed in callback) of root functor that maps to returned option
+     *
+     * @returns {function} option functor
+     */
+    ret.getLink = function(key) {
+        return links.get(key);
+    };
 
-	return ret;
+    /**
+     * Removes an option by key.
+     *
+     * @param {Object} key main result (as return or passed in callback) of root functor that maps to option to delete
+     *
+     * @returns {Boolean} true if deletion occured, false otherwise
+     */
+    ret.removeLink = function(key) {
+        return links.delete(key);
+    };
+
+    /**
+     * Applies first functor and the one which executes from the first functor's result. Works the same as function returned from constructor.
+     *
+     * @param {...Object} arguments launch arguments
+     *
+     * @returns {Object} a return from the first executed functor with callback (or last if none has callback)
+     */
+    ret.apply = function() {
+        if (links.size == 0)
+            return f(...argsF, ...arguments);
+        if (!wait) {
+            let res = f(...argsF, ...arguments);
+            return links.get(res.key)(...res.args);
+        }
+        return f(...argsF, ...arguments, links);
+    };
+
+    return ret;
 };
-
-//TODO: rewrite CyclicWrapper considering callbacks
 
 /**
  * Wrapper for Cyclic operations. Repeats functor as long as predicate returns true.
- * Functor must accept the following: ([...argsF], [...arguments]) on first iteration, and spread of second elem from its own return on the following iterations
- * Predicate must accept [] on first check, and the first elem from the return of functor on the following checks
+ *
+ *
  * @kind class
- * @param {functor} functor a functor to be executed repeatedly
- * @param {predicate} predicate a predicate to check whether to continue cycling
- * @param {...Object} argsF any preset arguments for functor
+ *
+ *
+ * @param {Object} functorInfo consists of the following (anything except f can be omit):
+ * * @param {function} f a functor to be called repeatedly
+ * * @param {Boolean} waitF true if functor has a callback, false otherwise
+ * * @param {Array} argsF preset arguments for the first call of functor
+ *
+ * If functor doesn't have a callback, it must match this scheme:
+ * * @param {...Object} toFunctor (on first iteration --- argsF + .apply() arguments)
+ * * @returns {Object} { toPredicate: Array, toFunctor: Array } those are the arguments for future iterations
+ *
+ * If functor has a callback, it must match this scheme:
+ * * @param {Array} toFunctor (on first iteration --- argsF + .apply() arguments)
+ * * @param {function} predicate_clb predicate based on predicate from this Wrapper
+ * Note: it must pass arguments for future iterations (toPredicate, toFunctor) to the predicate_clb in given order as arrays as 2 arguments on finish of callback execution
+ *
+ *
+ * @param {Object} predicateInfo consists of the following (anything except p can be omit):
+ * * @param {function} p a predicate. Must return true if continue cycle, false otherwise
+ * * @param {Boolean} waitP true if predicate has a callback, false otherwise
+ * * @param {Array} argsP preset arguments for the first call of predicate
+ *
+ * If predicate doesn't have a callback, it must match this scheme:
+ * * @param {...Object} toPredicate (on first iteration --- argsP)
+ * * @returns {Boolean} true if keep iterating, false otherwise
+ *
+ * If predicate has a callback, it must match this scheme:
+ * * @param {Array} toPredicate (on first iteration --- argsP)
+ * * @param {function} functor_clb functor with no arguments based on functor from this Wrapper. Call it on finish of callback execution, if iteration continues
+ *
+ *
  * @returns {function} function so Wrapper can be used as functor itself
  */
-function CyclicWrapper(functor, waitF, predicate, waitP, ...argsF) {
-	var ret = function() {
-		return ret.apply(...arguments);
-	};
+function CyclicWrapper(functorInfo, predicateInfo) {
 
-	functor = functor || ( () => {} );
-	predicate = predicate || ( () => false );
+    var f     = functorInfo.f,
+        waitF = functorInfo.waitF || false,
+        argsF = functorInfo.argsF || [];
 
-	/**
-	 * Applies functor as long as predicate returns true
-	 * @param {...Object} arguments any additional arguments for functor
-	 * @returns {Object} a pair which is returned from last execution of functor
-	 */
-	ret.apply = function() {
-		var obj = { toPredicate: [], toFunctor: (argsF || []) + (arguments || []) };
-		while (predicate(...obj.toPredicate))
-			obj = functor(...obj.toFunctor);
-		return obj;
-	};
+    var p     = predicateInfo.p,
+        waitP = predicateInfo.waitP || false,
+        argsP = predicateInfo.argsP || [];
 
-	return ret;
+    var ret = function CyclicWrapper() {
+        return ret.apply(...arguments);
+    };
+
+    /**
+     * Sets new functor.
+     *
+     * @param {function} new_f new functor
+     * @param {Boolean} new_waitF true if new functor has a callback, false otherwise
+     * @param {...Object} new_argsF preset arguments for the first call of new functor
+     *
+     * If new functor doesn't have a callback, it must match this scheme:
+     * * @param {...Object} toFunctor (on first iteration --- new_argsF + .apply() arguments)
+     * * @returns {Object} { toPredicate: Array, toFunctor: Array } those are the arguments for future iterations
+     *
+     * If new functor has a callback, it must match this scheme:
+     * * @param {Array} toFunctor (on first iteration --- new_argsF + .apply() arguments)
+     * * @param {function} predicate_clb predicate based on predicate from this Wrapper
+     * Note: it must pass arguments for future iterations (toPredicate, toFunctor) to the predicate_clb in given order as arrays as 2 arguments on finish of callback execution
+     *
+     * @returns {CyclicWrapper} this Wrapper
+     */
+    ret.setFunctor = function(new_f, new_waitF, ...new_argsF) {
+        { f, waitF, argsF } = { new_f, new_waitF, new_argsF };
+        return ret;
+    };
+
+    /**
+     * Sets new predicate.
+     *
+     * @param {function} new_p new predicate
+     * @param {Boolean} new_waitP true if new predicate has a callback, false otherwise
+     * @param {...Object} new_argsP preset arguments for the first call of new predicate
+     *
+     * If new predicate doesn't have a callback, it must match this scheme:
+     * * @param {...Object} toPredicate (on first iteration --- new_argsP, result from functor on following iterations)
+     * * @returns {Boolean} true if keep iterating, false otherwise
+     *
+     * If new predicate has a callback, it must match this scheme:
+     * * @param {Array} toPredicate (on first iteration --- new_argsP, result from functor on following iterations)
+     * * @param {function} functor_clb functor with no arguments based on functor from this Wrapper. Call it on finish of callback execution, if iteration continues
+     *
+     * @returns {CyclicWrapper} this Wrapper
+     */
+    ret.setPredicate = function(new_p, new_waitP, ...new_argsF) {
+        { p, waitP, argsP } = { new_p, new_waitP, new_argsP };
+        return ret;
+    };
+
+    /**
+     * Applies functor as long as predicate returns true. Works the same as function returned from constructor.
+     *
+     * @param {...Object} arguments launch arguments for functor
+     *
+     * @returns {Object} a pair which is returned from last execution of functor or null if at least one of functions have callbacks
+     */
+    ret.apply = function() {
+        if (!waitF && !waitP) {
+            var obj = {
+                toPredicate: argsP,
+                toFunctor: (argsF || []) + (arguments || [])
+            };
+            while (p(...obj.toPredicate))
+                obj = f(...obj.toFunctor);
+            return obj;
+        }
+
+        var packedP;
+        if (!waitF) {
+            packedP = (toPredicate, toFunctor) => {
+                p(toPredicate, () => {
+                    var obj = f(...toFunctor);
+                    packedP(obj.toPredicate, obj.toFunctor);
+                });
+            };
+        } else if (!waitP) {
+            packedP = (toPredicate, toFunctor) => {
+                if (p(...toPredicate))
+                    f(toFunctor, packedP);
+            };
+        } else {
+            packedP = (toPredicate, toFunctor) => {
+                p(toPredicate, () => {
+                    f(toFunctor, packedP);
+                });
+            };
+        }
+        packedP(argsP, argsF + arguments);
+    };
+
+    return ret;
 };
 
 if (module)
-	module.exports = {
-		LinearWrapper: LinearWrapper,
-		ScenarioWrapper: ScenarioWrapper,
-		CyclicWrapper: CyclicWrapper
-	};
+    module.exports = {
+        LinearWrapper: LinearWrapper,
+        ScenarioWrapper: ScenarioWrapper,
+        CyclicWrapper: CyclicWrapper
+    };
